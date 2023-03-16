184882b9a1c44f6a274c0a4a7fdb12b903243934
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-03-16 22:47:02 +1100

    adding some styling

--- assets/JS/highscorescript.js
+++ assets/JS/highscorescript.js
@@ -1,9 +1,21 @@
 var highscoreList = document.querySelector("#highscore-list");
+var clearButton = document.querySelector("#clear-button");
 
-var highScores = JSON.parse(localStorage.getItem("newScore"));
+var highScores = JSON.parse(localStorage.getItem("highScores"));
 
 console.log(highScores);
 
+for (i = 0; i < highScores.length; i ++) {
+    console.log("name: " + highScores[i].name);
+    console.log("score: " + highScores[i].point);
+    var score = document.createElement("p");
+    score.textContent = "name: " + highScores[i].name + "     " + "points: " + highScores[i].point;
+    highscoreList.appendChild(score);
+}
 
-// highscoreList.textContent = "name: " + highscores.name+ "     " + "points: " + highscores.point;
 
+clearButton.addEventListener("click", function (event) {
+    localStorage.clear();
+    highscoreList.innerHTML = "";
+    
+});
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,13 @@
 var startButton = document.querySelector("#start");
 var timerElement = document.querySelector("#time-reamaining");
-var main = document.querySelector("#quiz-area");
+var quizArea = document.querySelector("#quiz-area");
 var questionP = document.createElement("p");
 var submitButton = document.createElement("button");
 var nameInput = document.createElement("input");
 var submitP = document.createElement("p");
 var numberPoints = document.createElement("p");
 
-var highScores = JSON.parse(localStorage.getItem("newScore"));
+var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 var timeLeft;
 var timer;
@@ -18,18 +18,44 @@ var i =0;
 
 var questions = [
     {
-    question: "Here is a question",
-    answer1: "answer 1",
-    answer2: "answer 2",
-    answer3: "answer 3",
+    question: "What are HTML semantic elements?",
+    answer1: "Semantic elements, like `<div>`, hold the important content together so it's easy to understand.",
+    answer2: "Semantic elements are outdated and are no longer used in HTML.",
+    answer3: "A semantic element clearly describes its meaning to both the browser and the developer.",
+    answer4: "A semantic element reveals nothing about its content to the browser or the developer.",
+    correctAnswer: "3"
+    },
+    {
+    question: "What is the difference between && and ||?",
+    answer1: "The logical operator && returns true if both expressions are true while the logical operator || returns false if one expression or the other returns true.",
+    answer2: "The logical operator && returns true if both expressions are true while the logical operator || returns true if one expression or the other returns true.",
+    answer3: "The logical operator && returns true if one expression is true while the logical operator || returns true if both expressions returntrue true.",
+    answer4: "The logical operator && returns true if none of the expressions are true while the logical operator || returns true if one expression or the other returns true.",
+    correctAnswer: "2"
+    },
+    {
+    question: "What is a media query?",
+    answer1: "A feature of CSS3 allowing content rendering to adapt to different conditions such as screen resolution",
+    answer2: "A feature of JavaScript allowing content rendering to adapt to different conditions such as screen resolution",
+    answer3: "A feature of HTML allowing content rendering to adapt to different conditions such as screen resolution",
+    answer4: "A feature of Flexbox allowing content rendering to adapt to different conditions such as screen resolution",
     correctAnswer: "1"
     },
     {
-    question: "Here is another question",
-    answer1: "answer2 1",
-    answer2: "answer2 2",
-    answer3: "answer2 3",
-    correctAnswer: "3"
+    question: "Which property can you use in order to implement event delegation?",
+    answer1: "event.addEventListener()",
+    answer2: "event.stopPropagation()",
+    answer3: "event.preventDefault()",
+    answer4: "event.target",
+    correctAnswer: "4"
+    },
+    {
+    question: "You just finished the feature that you've been working on a successfully merged your branch, feature-52. How would you delete branch, feature-52?",
+    answer1: "git checkout feature-52",
+    answer2: "git branch -d feature-52",
+    answer3: "git branch feature-52",
+    answer4: "git branch feature-52",
+    correctAnswer: "2"
     }
 ]
 
@@ -58,7 +84,7 @@ function startTimer () {
         if(timeLeft === 0) {
             //tell user they lost and ran out of time
             timerElement.textContent = timeLeft;
-            createSubmit();
+            // createSubmit();
             clearInterval(timer);
         }
 
@@ -67,7 +93,7 @@ function startTimer () {
 }
 
 
-main.addEventListener("click", function (event) {
+quizArea.addEventListener("click", function (event) {
     var element = event.target;
     if( element.nodeName !== "LI") {
         return;
@@ -94,7 +120,7 @@ function checkAnswer (answer) {
         correctAnswer = false;
     }
     i ++;
-    generateQuestion(i);
+    // generateQuestion(i);
 }
 
 function addPoint() {
@@ -118,7 +144,7 @@ submitButton.addEventListener("click", function(event) {
         highScores.push(newScore);  
         console.log(highScores);
         localStorage.setItem("highScores", JSON.stringify(highScores));  
-        // window.location.href = "./highscore.html";
+        window.location.href = "./highscore.html";
     }
 
     
@@ -137,33 +163,38 @@ function generateQuestion(i) {
         clearInterval(timer);
         createSubmit();
     } else {
-        //removing the children of main
-        main.innerHTML = "";
+        //removing the children of quizArea
+        quizArea.innerHTML = "";
         var questionHeading = document.createElement("ul");
         var answerP1 = document.createElement("li");
         var answerP2 = document.createElement("li");
         var answerP3 = document.createElement("li");
+        var answerP4 = document.createElement("li");
 
         questionHeading.textContent = questions[i].question;
         answerP1.textContent = questions[i].answer1;
         answerP2.textContent = questions[i].answer2;
         answerP3.textContent = questions[i].answer3;
+        answerP4.textContent = questions[i].answer4;
 
-        main.appendChild(questionHeading);
-        main.appendChild(answerP1);
-        main.appendChild(answerP2);
-        main.appendChild(answerP3);
-        main.appendChild(questionP);
+        quizArea.appendChild(questionHeading);
+        quizArea.appendChild(answerP1);
+        quizArea.appendChild(answerP2);
+        quizArea.appendChild(answerP3);
+        quizArea.appendChild(answerP4);
+        quizArea.appendChild(questionP);
 
 
         questionHeading.setAttribute("data-type", "question")
         answerP1.setAttribute("data-number", "1");
         answerP2.setAttribute("data-number", "2");
         answerP3.setAttribute("data-number", "3");
+        answerP3.setAttribute("data-number", "4");
         questionHeading.setAttribute("class", "question");
         answerP1.setAttribute("class", "answer");
         answerP2.setAttribute("class", "answer");
         answerP3.setAttribute("class", "answer");
+        answerP4.setAttribute("class", "answer");
     }
 
 
@@ -171,14 +202,15 @@ function generateQuestion(i) {
 }
 
 function createSubmit () {
-    main.innerHTML = "";
+    quizArea.innerHTML = "";
     submitButton.textContent = "Submit";
     submitP.textContent = "Please enter you initals";
     numberPoints.textContent = "Congratulations, you earned " + point + " points!";
-    main.appendChild(numberPoints);
-    main.appendChild(submitP);
-    main.appendChild(nameInput);
-    main.appendChild(submitButton);
+    quizArea.appendChild(numberPoints);
+    quizArea.appendChild(submitP);
+    quizArea.appendChild(nameInput);
+    quizArea.appendChild(submitButton);
     nameInput.setAttribute("id", "name");
     submitButton.setAttribute("id", "submit");
+    submitButton.setAttribute("class", "btn");
 }
